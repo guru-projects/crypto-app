2fe399c865b861d8a361d6e7e020fec2ace252aa
Guru <guru85111@gmail.com>
2024-08-12 20:08:06 +0530

    "Added @splinetool/react-spline dependency and imported Spline component in Banner.jsx, updated Banner component with new layout and design, removed bg.jpg file and updated package.json and package-lock.json files accordingly."

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,14 +3,15 @@ import {
     responsiveFontSizes,
     ThemeProvider,
   } from '@mui/material/styles';
-  import Typography from '@mui/material/Typography';
+import Typography from '@mui/material/Typography';
+import Spline from '@splinetool/react-spline';
 import { Container } from '@mui/material';
 import Carousel from './Carousel';
   
   let theme = createTheme({
     typography: {
     h2:{
-        fontWeight: 600
+        fontWeight: 700
     },
 }
   });
@@ -20,23 +21,27 @@ import Carousel from './Carousel';
 // Banner Component
 const Banner = () => {
   return (
-    <div className="bg-[url('../../bg.jpg')] h-96">
+    <div className="bg-[url('../../bg3.jpg')] min-h-96 bg-cover bg-no-repeat">
         <ThemeProvider theme={theme} >
           <Container sx={{
             display: 'flex',
             flexDirection: 'column',
+            paddingTop: '100px',
             justifyContent: 'space-around',
             height: '100%', // Optional: Assuming you want white text over the background
           }}>
-            <div className='text-center'>
-                <Typography variant="h2" >
+            <div className='text-center z-10 mt-40 text-slate-900'>
+                <Typography variant="h2">
                   Welcome to Crypto App
                 </Typography>
                 <Typography variant="subtitle2">
                   This is a simple React app to fetch cryptocurrency prices.
                 </Typography>
             </div>
-            <Carousel />
+            <div className='lg:h-screen absolute pl-20 lg:w-max overflow-hidden -mt-80 lg:-mt-20  -ml-80 md:-ml-20 lg:ml-60'>
+              <Spline scene="https://prod.spline.design/9WtVgbVcF7JH2oKo/scene.splinecode" />
+            </div>
+            <div className='mt-80 mb-10'><Carousel/></div>
           </Container>
         </ThemeProvider>
     </div>
